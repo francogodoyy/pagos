b9d6e2cddb241d1eb5dc0aa32dcf0b15971576bf
Franco <alanfrancogodoy@gmail.com>
2025-01-25 20:33:04 -0300

    committt

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true, // Si estás usando la estructura de rutas con el directorio 'app'
-    },
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
