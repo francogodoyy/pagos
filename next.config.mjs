42ba1c5205a038b76065c1945bd03e825f3b7ca4
francogodoyy <alanfrancogodoy@gmail.com>
2026-06-12 12:24:03 -0300

    added sentry

--- next.config.mjs
+++ next.config.mjs
@@ -12,4 +12,14 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const { withSentryConfig } = await import("@sentry/nextjs");
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG || "my-org",
+  project: process.env.SENTRY_PROJECT || "my-project",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
